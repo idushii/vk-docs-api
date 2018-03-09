0fa6d914b25d392dc02e055206e7e7e0faf18771
Nicolay <nic.nic343@ya.ru>
2018-03-09 21:52:20 +0700

    Добавить сортировку

--- src/DocsPage.vue
+++ src/DocsPage.vue
@@ -5,10 +5,10 @@
         <h5 class="card-title">Документы</h5>
       </div>
         <div class="row m-0">
-          <div class="col-7 border-bottom border-top p-2 text-center"><b>Название</b></div>
-          <div class="col border-bottom border-top p-2 text-center"><b>Дата</b></div>
-          <div class="col border-bottom border-top p-2 text-center"><b>Размер</b></div>
-          <div class="col border-bottom border-top p-2 text-center"><b></b></div>
+          <div class="col-7 caption-table" :class="{'sort-title': sortField == 'title'}" @click="sort('title')"><b>Название</b></div>
+          <div class="col caption-table" :class="{'sort-title': sortField == 'date'}" @click="sort('date')"><b>Дата</b></div>
+          <div class="col caption-table" :class="{'sort-title': sortField == 'size'}" @click="sort('size')"><b>Размер</b></div>
+          <div class="col caption-table"><b></b></div>
           <template  v-for="doc in docs">
             <div class="col-7 border-bottom border-top p-2 pl-4" :key="`doc1-${doc.id}`"><a :href="doc.url">{{doc.title}}</a></div>
             <div class="col border-bottom border-top p-2 text-truncate" :key="`doc2-${doc.id}`">{{doc.date | date}}</div>
@@ -29,17 +29,47 @@ export default {
   mixins: [docs_mixin],
   name: "DocsPage",
   data() {
-    return {};
+    return {
+      sortField: 'date',
+      order: 'min'
+    };
   },
   computed: {},
   mounted() {},
-  methods: {}
+  methods: {
+    sort(field = 'date') {
+      if (this.sortField == field) this.order = this.order == 'min' ? 'max' : 'min';
+      let order = this.order;
+      this.sortField = field
+      this.docs.sort(function(d1, d2) {
+        let n = order == 'min' ? 1 : -1
+        if (field == 'date') return d1.date*1 > d2.date*1 ? n : -n;
+        if (field == 'title') return d1.title > d2.title ? n : -n;
+        if (field == 'size') return d1.size*1 > d2.size*1 ? n : -n;
+      })
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
+  $color-border:  rgb(224, 209, 209);
+  $sort-title: $color-border;
+
   a:link, a:visited {
     color: rgb(51, 45, 45);
+  }
+
+  .sort-title {
+    background: $sort-title;
+  }
+  .caption-table {
+    border-top: 1px solid $color-border;
+    border-bottom: 1px solid $color-border;
+    padding: .5rem;
+    text-align: center;
+    cursor: pointer;
+    transition: all 0.2s;
   }
 </style>
