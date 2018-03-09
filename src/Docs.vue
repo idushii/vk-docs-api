e9faecb8e0e4b4798832dc3fb01831079a228136
Nicolay <nic.nic343@ya.ru>
2018-03-09 22:23:37 +0700

    ++

--- src/Docs.vue
+++ src/Docs.vue
@@ -5,8 +5,8 @@
       <template v-for="doc in last">
         <a :key="`doc-${doc.id}`" :href="doc.url" class="list-group-item list-group-item-action flex-column align-items-start">
           <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">{{doc.title}}</h5>
-            <small>{{doc.date | date}}</small>
+            <h5 class="mb-1 text-truncate">{{doc.title}}</h5>
+            <small class="date">{{doc.date | date}}</small>
           </div>
           <small>Файл {{doc.ext}}</small>
           <small class="float-right">размер {{doc.size | size}}</small>
@@ -45,6 +45,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-
+<style scoped>
+  .date { min-width: 100px; text-align: right; }
 </style>
